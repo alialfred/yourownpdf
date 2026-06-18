4b4b9a5e50a2b93487c7cda90b8438dcd409d115
Goro Daimon <goro.daimon78@gmail.com>
2026-06-18 21:20:54 +0500

    Fix File Not Available error by passing ignoreEncryption: true on PDF loads

--- assets/__vite-browser-external-CW-JKuqH.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./index-OpfdNQcy.js";var t=e(((e,t)=>{t.exports={}}));export{t};
---
+++ assets/__vite-browser-external-CZMa0ak8.js
@@ -0,0 +1,1 @@
+import{m as e}from"./index-Dl7whju7.js";var t=e(((e,t)=>{t.exports={}}));export{t};
