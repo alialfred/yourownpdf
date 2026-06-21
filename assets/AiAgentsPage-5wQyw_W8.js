280a3a779e1b41c31b18bb0181403915d2214ffa
Goro Daimon <goro.daimon78@gmail.com>
2026-06-21 18:28:57 +0500

    Deploy: Promote Remove Pages PDF tool to production (Remove Beta Labels and prioritize sorting)

--- assets/AiAgentsPage-5wQyw_W8.js
+++ assets/AiAgentsPage-5wQyw_W8.js
@@ -1,4 +1,4 @@
-import{f as e,p as t,y as n}from"./index--1QDRLD_.js";var r=n(t(),1),i=e(),a=`{
+import{f as e,p as t,y as n}from"./index-CATnZOrJ.js";var r=n(t(),1),i=e(),a=`{
   "mcpServers": {
     "yourownpdf-agent-tools": {
       "command": "npx",
--- assets/__vite-browser-external-DGRGSUBS.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./index--1QDRLD_.js";var t=e(((e,t)=>{t.exports={}}));export{t};
